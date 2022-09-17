40267364348122892b4986f8ba5e797b984a644a
depsztyczek <wojtek.k1999@gmail.com>
2022-09-17 20:57:17 +0200

    Grouped confif.h definitions

--- src/config.h
+++ src/config.h
@@ -7,20 +7,32 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+// Network and MQTT configurations
 #ifdef CONFIG_NET_CONFIG_SETTINGS
-#define ZEPHYR_ADDR		CONFIG_NET_CONFIG_MY_IPV4_ADDR
-#define SERVER_ADDR		CONFIG_NET_CONFIG_PEER_IPV4_ADDR
+#define ZEPHYR_ADDR		        CONFIG_NET_CONFIG_MY_IPV4_ADDR
+#define SERVER_ADDR		        CONFIG_NET_CONFIG_PEER_IPV4_ADDR
 #endif
 
-#define SERVER_PORT		1883
+#define SERVER_PORT		        1883
 
 #define APP_CONNECT_TIMEOUT_MS	2000
-#define APP_SLEEP_MSECS		500
+#define APP_SLEEP_MSECS		    500
 
-#define APP_CONNECT_TRIES	10
+#define APP_CONNECT_TRIES	    10
 
 #define APP_MQTT_BUFFER_SIZE	128
 
-#define MQTT_CLIENTID		"zephyr_publisher"
+#define MQTT_CLIENTID		    "zephyr_publisher"
+
+// Threads configurations
+#define STACK_SIZE              2048
+#define ADC_PRIORITY            6
+#define MQTT_PRIORITY           7
+
+// ADC configurations
+#define ADC_RESOLUTION		    12
+#define ADC_GAIN		        ADC_GAIN_1
+#define ADC_REFERENCE		    ADC_REF_INTERNAL
+#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
 
 #endif
